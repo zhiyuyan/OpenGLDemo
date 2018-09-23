acec4325a9f287fe0d9e7f568edbcdd45d044802
yanzy <1187997355@qq.com>
2018-09-23 20:36:22 +0800

    hello world

---
+++ app/src/main/java/com/android/yzy/opengldemo/IOpenGLDemo.java
@@ -0,0 +1,13 @@
+package com.android.yzy.opengldemo;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by yzy on 2018/9/23.
+ */
+
+public interface IOpenGLDemo {
+
+    public void DrawScene(GL10 gl);
+
+}
--- app/src/main/java/com/android/yzy/opengldemo/MainActivity.java
+++ app/src/main/java/com/android/yzy/opengldemo/MainActivity.java
@@ -1,13 +1,120 @@
 package com.android.yzy.opengldemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends ListActivity {
+
+    private static final String CategoryName = "intent.category.opengl.SAMPLE_CODE";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setListAdapter(new SimpleAdapter(this, getData(),
+                android.R.layout.simple_list_item_1, new String[]{"title"},
+                new int[]{android.R.id.text1}));
+        getListView().setTextFilterEnabled(true);
+
     }
+
+    protected List getData() {
+        List<Map> myData = new ArrayList<>();
+
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(CategoryName);
+
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(mainIntent, 0);
+
+        if (null == list)
+            return myData;
+
+        String[] prefixPath;
+
+        prefixPath = null;
+
+        int len = list.size();
+
+        Map<String, Boolean> entries = new HashMap<>();
+
+        for (int i = 0; i < len; i++) {
+            ResolveInfo info = list.get(i);
+            CharSequence labelSeq = info.loadLabel(pm);
+            String label = labelSeq != null ? labelSeq.toString()
+                    : info.activityInfo.name;
+
+            String[] labelPath = label.split("/");
+
+            String nextLabel = prefixPath == null ? labelPath[0]
+                    : labelPath[prefixPath.length];
+
+            if ((prefixPath != null ? prefixPath.length : 0) == labelPath.length - 1) {
+                addItem(myData,
+                        nextLabel,
+                        activityIntent(
+                                info.activityInfo.applicationInfo.packageName,
+                                info.activityInfo.name));
+            } else {
+                if (entries.get(nextLabel) == null) {
+                    addItem(myData, nextLabel, browseIntent(nextLabel));
+                    entries.put(nextLabel, true);
+                }
+            }
+
+        }
+
+        Collections.sort(myData, sDisplayNameComparator);
+
+        return myData;
+    }
+
+    private final static Comparator<Map> sDisplayNameComparator = new Comparator<Map>() {
+        private final Collator collator = Collator.getInstance();
+
+        public int compare(Map map1, Map map2) {
+            return collator.compare(map1.get("title"), map2.get("title"));
+        }
+    };
+
+    protected Intent activityIntent(String pkg, String componentName) {
+        Intent result = new Intent();
+        result.setClassName(pkg, componentName);
+        return result;
+    }
+
+    protected Intent browseIntent(String path) {
+        Intent result = new Intent();
+        result.setClass(this, MainActivity.class);
+        return result;
+    }
+
+    protected void addItem(List<Map> data, String name, Intent intent) {
+        Map<String, Object> temp = new HashMap<String, Object>();
+        temp.put("title", name);
+        temp.put("intent", intent);
+        data.add(temp);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Map map = (Map) l.getItemAtPosition(position);
+        Intent intent = (Intent) map.get("intent");
+        startActivity(intent);
+    }
+
 }
