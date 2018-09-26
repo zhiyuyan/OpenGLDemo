05f344b83663780eca25dc84b370bcfb3d967c50
yanzy <1187997355@qq.com>
2018-09-26 13:04:59 +0800

    Draw line

---
+++ app/src/main/java/com/android/yzy/opengldemo/DrawLineActivity.java
@@ -0,0 +1,65 @@
+package com.android.yzy.opengldemo;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by yzy on 2018/9/26.
+ */
+
+public class DrawLineActivity extends OpenGLESActivity {
+
+    float vertexArray[] = {
+            -0.8f, -0.4f * 1.732f, 0.0f,
+            -0.4f, 0.4f * 1.732f, 0.0f,
+            0.0f, -0.4f * 1.732f, 0.0f,
+            0.4f, 0.4f * 1.732f, 0.0f
+    };
+
+    long mLastTime;
+
+    int index;
+
+    @Override
+    public void DrawScene(GL10 gl) {
+        super.DrawScene(gl);
+
+        ByteBuffer vbb = ByteBuffer.allocateDirect(vertexArray.length * 4);
+        vbb.order(ByteOrder.nativeOrder());
+        FloatBuffer vertexBuffer = vbb.asFloatBuffer();
+        vertexBuffer.put(vertexArray);
+        vertexBuffer.position(0);
+
+        gl.glLoadIdentity();
+        gl.glTranslatef(0f,0f,-4f);
+
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        long now = System.currentTimeMillis();
+        if (now - mLastTime > 2000) {
+            mLastTime = now;
+            index++;
+            index%=3;
+        }
+        switch (index) {
+            case 0:
+                gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+                gl.glDrawArrays(GL10.GL_LINES, 0, 4);
+                break;
+            case 1:
+                gl.glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+                gl.glDrawArrays(GL10.GL_LINE_STRIP, 0, 4);
+                break;
+            case 2:
+                gl.glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+                gl.glDrawArrays(GL10.GL_LINE_LOOP, 0, 4);
+                break;
+        }
+
+        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+    }
+}
