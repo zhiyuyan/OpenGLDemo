d8904b20f7f211409c5bcedf273afd6af616b26d
yanzy <1187997355@qq.com>
2018-11-11 21:21:13 +0800

    使用varying变量进行平滑着色

--- app/src/main/java/com/android/yzy/opengldemo/render/AirHockeyRenderer.java
+++ app/src/main/java/com/android/yzy/opengldemo/render/AirHockeyRenderer.java
@@ -22,16 +22,17 @@ import javax.microedition.khronos.opengles.GL10;
 public class AirHockeyRenderer implements GLSurfaceView.Renderer{
 
     private static final int POSITION_COMPONENT_COUNT = 2;
-
+    private static final int COLOR_COMPONENT_COUNT = 3;
     private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
     private Context mContext;
 
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
-
     private static final String A_POSITION = "a_Position";
     private int aPositionLocation;
+
+    private static final String A_COLOR = "a_Color";
+    private int aColor;
 
     private int mProgram;
 
@@ -44,29 +45,28 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer{
         // 准备顶点数据
         float[] tableVertices = {
                 // Triangle
-                0f, 0f,
-                -0.5f, -0.5f,
-                0.5f, -0.5f,
-                0.5f, 0.5f,
-                -0.5f, 0.5f,
-                -0.5f, -0.5f,
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
 
                 // Line
-                -0.5f, 0f,
-                0.5f, 0f,
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
 
                 // Point 1
-                0f, -0.25f,
+                0f, -0.25f, 0f, 0f, 1f,
 
                 // Point 2
-                0f, 0.25f
+                0f, 0.25f, 1f, 0f, 0f
         };
 
         FloatBuffer vertexData = ByteBuffer.allocateDirect(tableVertices.length * BYTES_PER_FLOAT)
                 .order(ByteOrder.nativeOrder())
                 .asFloatBuffer();
         vertexData.put(tableVertices);
-        vertexData.position(0);
 
         // 设置背景颜色
         GLES20.glClearColor(0.0f,0.0f, 0.0f, 0.0f);
@@ -90,12 +90,16 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer{
         // 使用OpenGL程序对象
         GLES20.glUseProgram(mProgram);
 
-        uColorLocation = GLES20.glGetUniformLocation(mProgram, U_COLOR);
+        vertexData.position(0);
         aPositionLocation = GLES20.glGetAttribLocation(mProgram, A_POSITION);
-        GLES20.glEnableVertexAttribArray(aPositionLocation);
         GLES20.glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
-                false, 0, vertexData);
+                false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aPositionLocation);
 
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        aColor = GLES20.glGetAttribLocation(mProgram, A_COLOR);
+        GLES20.glVertexAttribPointer(aColor, COLOR_COMPONENT_COUNT, GLES20.GL_FLOAT, false, STRIDE, vertexData);
+        GLES20.glEnableVertexAttribArray(aColor);
     }
 
     @Override
@@ -110,18 +114,14 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer{
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
         // 绘制桌子
-        GLES20.glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, 6);
 
         // 绘制分隔线
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_LINES, 6, 2);
 
         // 绘制点
-        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 8, 1);
 
-        GLES20.glUniform4f(uColorLocation, 1.0f, 0.0f, 1.0f, 1.0f);
         GLES20.glDrawArrays(GLES20.GL_POINTS, 9, 1);
     }
 }
